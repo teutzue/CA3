17200e5c5f941323e6e9e62062793bdf0322598d
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2015-11-25 21:16:53 +0100

    delete web/*

--- web/app/view4/view4.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('myApp.view4', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view4', {
-    templateUrl: 'app/view4/view4.html'
-  });
-}]);
