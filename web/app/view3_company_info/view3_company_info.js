baae6d175a4fd2a543f34afa08a3a8abc054cfd4
expert26111 <djoana.v@gmail.com>
2015-11-26 11:41:32 +0200

    djfkg

--- web/app/view3_company_info/view3_company_info.js
+++ web/app/view3_company_info/view3_company_info.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view3_company_info', ['ngRoute'])
+angular.module('myApp.view3_company_info', ['ngRoute','ngAnimate', 'ui.bootstrap'])  
 
     .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.when('/view3_company_info', {
@@ -10,13 +10,48 @@ angular.module('myApp.view3_company_info', ['ngRoute'])
         }])
 
     .controller('view3_company_infoCtrl', function ($http, $scope) {
-        $http({
-            method: 'GET',
-            url: 'api/demouser'
-        }).then(function successCallback(res) {
-            $scope.data = res.data.message;
-        }, function errorCallback(res) {
-            $scope.error = res.status + ": " + res.data.statusText;
-        });
+        
+        
+         
+                        $scope.company = {};
+                        $scope.productionunits=[];
+                        $scope.myvalue = false;
+                        $scope.property={};
+                        
+                           $scope.findCompany = function ()
+                        {
+                            $http({
+                                method: 'GET',
 
-    });
+                                url:'http://cvrapi.dk/api?search='+$('#find').val()+'&country=dk'   
+                               
+                           }).then(function successCallback(response) {
+                               $scope.company = response.data;
+                                $scope.productionunits=response.data.productionunits;
+                               console.log($scope.vat);
+                                console.log(response);
+                               console.log($scope.company);
+                                console.log( $scope.productionunits);
+                                
+
+                            }, function errorCallback(response) {
+                                alert("Error occured");
+                            });
+                        };
+                        
+
+                        $scope.showAlert = function () {
+                            $scope.myvalue = true;
+                        };
+
+                    });
+//        $http({
+//            method: 'GET',
+//            url: 'api/demouser'
+//        }).then(function successCallback(res) {
+//            $scope.data = res.data.message;
+//        }, function errorCallback(res) {
+//            $scope.error = res.status + ": " + res.data.statusText;
+//        });
+
+
