622288040c4f4f895826d7d085155b655e951a6b
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2015-11-28 17:08:18 +0100

    add features to view5 and view6

--- web/app/view5_all_users/view5_all_users.js
+++ web/app/view5_all_users/view5_all_users.js
@@ -2,20 +2,59 @@
 
 angular.module('myApp.view5_all_users', ['ngRoute'])
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view5_all_users', {
-            templateUrl: 'app/view5_all_users/view5_all_users.html',
-            controller: 'view5_all_usersCtrl'
-        });
-    }])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view5_all_users', {
+                    templateUrl: 'app/view5_all_users/view5_all_users.html',
+                    controller: 'view5_all_usersCtrl'
+                });
+            }])
 
-    .controller('view5_all_usersCtrl', function ($http, $scope) {
-        $http.get('api/demoadmin')
-            .success(function (data, status, headers, config) {
-                $scope.data = data;
-            })
-            .error(function (data, status, headers, config) {
+        .controller('view5_all_usersCtrl', function ($http, $scope) {
 
-            });
+            $scope.users;
 
-    });
+            // Get users :: Cliked
+            $scope.getUsers = function () {
+                $http.get('api/demoadmin/users')
+                        .success(function (data, status, headers, config) {
+
+                            $scope.users = data;                          
+                        })
+                        .error(function (data, status, headers, config) {
+
+                        });                           
+            }; // End of getUsers
+
+
+            // Delete user
+            $scope.delete = function (uName) {
+
+                $http.delete('api/demoadmin/users/' + uName)
+                        .success(function (data, status, headers, config) {
+
+                            var index = -1;
+                            var comArr = eval($scope.users);
+                            for (var i = 0; i < comArr.length; i++) {
+                                if (comArr[i].username === uName) {
+                                    index = i;
+                                    break;
+                                }
+                            }
+                            if (index === -1) {
+                                alert("Something gone wrong");
+                            }
+                            $scope.users.splice(index, 1);
+
+
+
+                        })
+                        .error(function (data, status, headers, config) {
+                            alert(data);
+                        });
+
+            }; // End of delete
+
+
+
+
+        }); // End of controller
--- web/app/view6_create_new_account/view6_create_new_account.js
+++ web/app/view6_create_new_account/view6_create_new_account.js
@@ -3,37 +3,37 @@
 angular.module('myApp.view6_create_new_account', ['ngRoute'])
 
         .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/view6_create_new_account', {
-                templateUrl: 'app/view6_create_new_account/view6_create_new_account.html',
-                controller: 'view6_create_new_accountCtrl'
-            });
-        }])
+                $routeProvider.when('/view6_create_new_account', {
+                    templateUrl: 'app/view6_create_new_account/view6_create_new_account.html',
+                    controller: 'view6_create_new_accountCtrl'
+                });
+            }])
 
-        .controller('view6_create_new_accountCtrl', function ($scope, $http ) {
-            
+        .controller('view6_create_new_accountCtrl', function ($scope, $http) {
+
             $scope.wrongPs = "visibility: hidden";
+            $scope.missingPS = "visibility: hidden";
 
             $scope.submit = function () {
 
-                if(checkPassword($scope.password, $scope.password2)) {
+                if (checkPassword($scope.password, $scope.password2) & checkUsername($scope.userName)) {
 
                     var credential = JSON.stringify({
-
-                            username: $scope.userName,
-                            password: $scope.password
-                        });
+                        username: $scope.userName,
+                        password: $scope.password
+                    });
 
                     var req = {
                         method: 'POST',
                         url: 'api/allusers',
-                        headers: { 'Content-Type': 'application/json' },
+                        headers: {'Content-Type': 'application/json'},
                         data: credential
                     };
 
                     $http(req).then(function successCallback(response) {
                         // this callback will be called asynchronously
                         // when the response is available
-                        if(response.data.isSaved === "yes" ) {
+                        if (response.data.isSaved === "yes") {
                             $scope.userName = "";
                             $scope.password = "";
                             $scope.password2 = "";
@@ -51,31 +51,55 @@ angular.module('myApp.view6_create_new_account', ['ngRoute'])
 
             }; // End of submit
 
-        $scope.Style = function (isSaved) {
-            
-            if(isSaved === "yes") {
-                return "color: green";
-            } else {
+            $scope.Style = function (isSaved) {
+
+                if (isSaved === "yes") {
+                    return "color: green";
+                } else {
 //                alert(isSaved);
-                return "color: red";
-            }
-        }; // End of Style
+                    return "color: red";
+                }
+            }; // End of Style
 
-        $scope.Focus = function () {
-            $scope.feetback = "";
-            $scope.feetbackError = "";
-        }; // End of Focus()
-
-        var checkPassword = function (pw1, pw2) {
-            
-            if( pw1 === pw2) {
+            $scope.Focus = function () {
+                $scope.feetback = "";
+                $scope.feetbackError = "";
                 $scope.wrongPs = "visibility: hidden";
+                $scope.missingPS = "visibility: hidden";
+            }; // End of Focus()
+
+            var checkPassword = function (pw1, pw2) {
+
+                if (checkForPassword(pw1)) {
+                    if (pw1 === pw2) {
+                        $scope.wrongPs = "visibility: hidden";
+                        return true;
+                    } else {
+                        $scope.wrongPs = "visibility: visible";
+                        return false;
+                    }
+                }
+            }; // End of checkPassword()
+
+
+            var checkForPassword = function (ps) {
+                if ((angular.isUndefined(ps)) | (ps === "")) {
+                    $scope.missingPS = "visibility: visible";
+                    return false;
+                } else {
+                    $scope.missingPS = "visibility: hidden";
+                    return true;
+                }
+
+            };
+
+            var checkUsername = function (username) {
+
+                if ((angular.isUndefined(username)) | (username === "")) {
+                    $scope.feetbackError = "Missing username";
+                    return false;
+                }
                 return true;
-            } else {
-                $scope.wrongPs = "visibility: visible";
-                return false;
-            }
-        }; // End of checkPassword()
+            };
 
-
-    }); // End of controller
+        }); // End of controller
